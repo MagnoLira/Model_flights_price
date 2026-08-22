58d6cb2b65e316d0c1d72b089e88c24fde3ca423
Magno Lira <silvalira968@gmail.com>
2026-08-22 17:32:18 -0300

    [ Update ] : removing true auto commit

--- database/kafka_topic_raw_consumer.py
+++ database/kafka_topic_raw_consumer.py
@@ -21,7 +21,7 @@ consumer = KafkaConsumer(
     bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
     value_deserializer=lambda m: json.loads(m.decode('utf-8')),
     auto_offset_reset='earliest',
-    enable_auto_commit=True,  # Não comita automaticamente
+    enable_auto_commit=False,  # Não comita automaticamente
     group_id='raw-flight-group-prod' 
 )
 
